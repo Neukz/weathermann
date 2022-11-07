d027ce32dbed03de8d9831000f296d1e564099c5
Kacper Neumann <kapilek31@gmail.com>
2022-11-07 21:47:32 +0100

    Create constants for outgoing links

---
+++ src/constants/links.ts
@@ -0,0 +1,7 @@
+// Outgoing links
+export const links = {
+	projectRepo: 'https://github.com/Neukz/weathermann',
+	proxyRepo: 'https://github.com/Neukz/neukz-api',
+	openWeatherMapAPI: 'https://openweathermap.org/api',
+	reactLeaflet: 'https://react-leaflet.js.org'
+};
