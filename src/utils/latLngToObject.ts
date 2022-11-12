64cd2594d35a0f70e9ab7041198e353d158bb7ab
Kacper Neumann <kapilek31@gmail.com>
2022-11-12 19:56:21 +0100

    Create util function for LatLng to object literal conversion

--- src/utils/latLngToObject.ts
+++ src/utils/latLngToObject.ts
@@ -1,16 +1,10 @@
 import { LatLngLiteral } from 'leaflet';
-
-// Use object literal interface as it's not recommended to use non-seriazable values in Redux
-// See: https://redux.js.org/faq/actions#why-should-type-be-a-string-or-at-least-serializable-why-should-my-action-types-be-constants
-export interface PositionSchema {
-	lat: number;
-	lng: number;
-}
+import { Position } from '../store/reducers/position';
 
 // Function to convert a LatLngLiteral instance to an object literal used in Redux store
 export const latLngToObject = (latlng: LatLngLiteral) => {
 	return {
 		lat: latlng.lat,
 		lng: latlng.lng
-	} as PositionSchema;
+	} as Position;
 };
