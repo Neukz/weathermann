32cad281cf9c75024751156357fb8bd650fcb35f
Kacper Neumann <kapilek31@gmail.com>
2022-11-17 21:00:32 +0100

    Create not found page

--- src/pages/NotFound/index.tsx
+++ src/pages/NotFound/index.tsx
@@ -1,5 +1,33 @@
+import Container from 'react-bootstrap/Container';
+import Image from 'react-bootstrap/Image';
+import Button from 'react-bootstrap/Button';
+import { useNavigate } from 'react-router-dom';
+
 const NotFound = () => {
-	return <div>NotFound</div>;
+	const navigate = useNavigate();
+
+	return (
+		<Container className="mt-3">
+			<header>
+				<h1 className="text-center">Page Not Found</h1>
+			</header>
+
+			<Image
+				className="d-block mx-auto"
+				src="https://img.icons8.com/cotton/256/000000/error-cloud.png"
+				alt=""
+			/>
+
+			<Button
+				variant="warning"
+				size="lg"
+				className="d-block mx-auto mt-3 px-5"
+				onClick={() => navigate(-1)}
+			>
+				Back
+			</Button>
+		</Container>
+	);
 };
 
 export default NotFound;
