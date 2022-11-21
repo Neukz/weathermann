c1c54ffb02cc19c16d7766e5ed417788bfdc24ff
Kacper Neumann <kapilek31@gmail.com>
2022-11-21 20:06:11 +0100

    Handle fetching weather data

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,25 +1,92 @@
+import { useEffect } from 'react';
 import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
+import Spinner from 'react-bootstrap/Spinner';
+import Alert from 'react-bootstrap/Alert';
 import Map from '../../components/Map';
+import { useNavigate } from 'react-router-dom';
+import { useAppSelector, useAppDispatch } from '../../hooks/reduxTypedHooks';
+import {
+	selectPosition,
+	selectWeather,
+	fetchWeather,
+	clearWeather,
+	selectAlert,
+	clearAlert,
+	clearError
+} from '../../store';
+import { useUnits } from '../../hooks/useUnits';
 
 const Home = () => {
+	const [units] = useUnits();
+	const { position } = useAppSelector(selectPosition);
+	const { loading, data, error } = useAppSelector(selectWeather);
+	const { alert } = useAppSelector(selectAlert);
+	const dispatch = useAppDispatch();
+
+	const navigate = useNavigate();
+
+	// In case user wants to go back from weather page
+	useEffect(() => {
+		dispatch(clearWeather());
+	}, []);
+
+	useEffect(() => {
+		if (data) {
+			navigate('/weather');
+		}
+	}, [data]);
+
+	const handleClick = () => {
+		dispatch(clearAlert());
+		dispatch(clearError());
+		// @ts-ignore - it won't be clickable if position is null
+		dispatch(fetchWeather({ position, units }));
+	};
+
 	return (
 		<Container fluid className="p-0 overflow-hidden">
-			<Row>
-				<Col>
-					<Map />
-				</Col>
-			</Row>
+			<Map />
 
-			<Row className="mt-3">
-				<Col xs={7} sm={5} md={4} lg={3} xxl={2} className="mx-auto">
-					<Button size="lg" className="w-100">
-						Search
-					</Button>
-				</Col>
-			</Row>
+			<Alert
+				className="w-75 mx-auto mt-3"
+				variant="warning"
+				dismissible
+				show={alert !== null}
+				onClose={() => dispatch(clearAlert())}
+			>
+				{alert}
+			</Alert>
+
+			<Alert
+				className="w-75 mx-auto mt-3"
+				variant="danger"
+				dismissible
+				show={error !== null}
+				onClose={() => dispatch(clearError())}
+			>
+				{error}
+			</Alert>
+
+			<Button
+				size="lg"
+				className="d-block mx-auto mt-3 px-5"
+				disabled={!position || loading}
+				onClick={handleClick}
+			>
+				{loading && (
+					<Spinner
+						className="align-baseline"
+						as="span"
+						size="sm"
+						animation="grow"
+						role="status"
+						aria-hidden="true"
+					/>
+				)}
+
+				{loading ? ' Loading...' : 'Get weather'}
+			</Button>
 		</Container>
 	);
 };
