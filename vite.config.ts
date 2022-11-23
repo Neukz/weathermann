2e40368a9280cfdcd9085a8cd67671eea264d23c
Kacper Neumann <kapilek31@gmail.com>
2022-11-23 23:05:35 +0100

    Fix react-moment pre-bundling

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { esbuildCommonjs } from '@originjs/vite-plugin-commonjs';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+	plugins: [react()],
+	optimizeDeps: {
+		esbuildOptions: {
+			// See: https://github.com/vitejs/vite/issues/6675
+			plugins: [esbuildCommonjs(['react-moment'])]
+		}
+	}
+});
