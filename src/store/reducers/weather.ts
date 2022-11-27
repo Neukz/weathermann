b31f147b7b0e93e040071a41ccd02486dc76a43e
Kacper Neumann <kapilek31@gmail.com>
2022-11-27 19:03:33 +0100

    Restructure weather state for air and forecast data

--- src/store/reducers/weather.ts
+++ src/store/reducers/weather.ts
@@ -4,8 +4,8 @@ import { Position } from './position';
 import { Units } from './units';
 import axios, { AxiosError } from 'axios';
 
-// Response schema from OpenWeatherMap API
-interface Response {
+// Responses from OpenWeatherMap API
+interface CurrentWeather {
 	coord: {
 		lon: number;
 		lat: number;
@@ -56,6 +56,77 @@ interface Response {
 	id: number;
 	name: string;
 	cod: number;
+}
+
+interface Air {
+	coord: {
+		lon: number;
+		lat: number;
+	};
+	list: [
+		{
+			dt: number;
+			main: {
+				aqi: number;
+			};
+			components: {
+				co: number;
+				no: number;
+				no2: number;
+				o3: number;
+				so2: number;
+				pm2_5: number;
+				pm10: number;
+				nh3: number;
+			};
+		}
+	];
+}
+
+interface Forecast {
+	cod: string;
+	message: number;
+	cnt: number;
+	list: [
+		{
+			dt: number;
+			main: CurrentWeather['main'] & {
+				temp_kf: number;
+			};
+			weather: CurrentWeather['weather'];
+			clouds: CurrentWeather['clouds'];
+			wind: CurrentWeather['wind'];
+			visibility: number;
+			pop: number;
+			rain: {
+				'3h': number;
+			};
+			sys: {
+				pod: 'd' | 'n';
+			};
+			dt_txt: string;
+		}
+	];
+	city: {
+		id: number;
+		name: string;
+		coord: {
+			lat: number;
+			lon: number;
+		};
+		country: string;
+		population: number;
+		timezone: number;
+		sunrise: number;
+		sunset: number;
+	};
+}
+
+// Response from proxy server
+interface Response {
+	current: CurrentWeather;
+	forecast: Forecast;
+	air: Air;
 }
 
 interface ResponseError {
