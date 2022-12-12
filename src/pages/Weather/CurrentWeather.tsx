3c540dfb4cae3eee1c27dc71821793386292cf88
Kacper Neumann <kapilek31@gmail.com>
2022-12-12 19:25:31 +0100

    Change layout

--- src/pages/Weather/CurrentWeather.tsx
+++ src/pages/Weather/CurrentWeather.tsx
@@ -3,7 +3,6 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Image from 'react-bootstrap/Image';
-import ListGroup from 'react-bootstrap/ListGroup';
 import Moment from 'react-moment';
 import { useAppSelector } from '../../hooks/reduxTypedHooks';
 import { selectUnits } from '../../store';
@@ -17,10 +16,16 @@ const CurrentWeather = () => {
 		<Card>
 			<Card.Header className="text-center">Current Weather</Card.Header>
 			<Card.Body>
-				<Container fluid className="p-0">
-					<Row md={2} className="overflow-hidden">
+				<Container className="p-0">
+					<Row>
 						<Col>
-							<Card.Title>
+							<Card.Title className="fs-3 d-flex align-items-center">
+								{current?.weather[0].icon && (
+									<Image
+										src={`https://openweathermap.org/img/wn/${current?.weather[0].icon}.png`}
+										alt=""
+									/>
+								)}
 								{current?.main.temp.toFixed(0)}
 								{weatherUnits.temp[units]}
 							</Card.Title>
@@ -29,57 +34,78 @@ const CurrentWeather = () => {
 								Feels like {current?.main.feels_like.toFixed(0)}
 								{weatherUnits.temp[units]}, {current?.weather[0].description}
 							</Card.Subtitle>
+						</Col>
 
-							<Card.Text className="mt-2 mb-0 text-muted">
-								Sunrise:{' '}
+						<Col className="text-nowrap">
+							<Card.Text className="mt-2 mb-0">
+								<i className="bi-sunrise" />{' '}
 								<Moment
 									utc
 									date={current?.sys.sunrise! * 1000}
 									add={{ seconds: current?.timezone }}
-									format="LT"
+									format="hh:mm A"
 								/>
 							</Card.Text>
 
-							<Card.Text className="text-muted">
-								Sunset:{' '}
+							<Card.Text>
+								<i className="bi-sunset" />{' '}
 								<Moment
 									utc
 									date={current?.sys.sunset! * 1000}
 									add={{ seconds: current?.timezone }}
-									format="LT"
+									format="hh:mm A"
 								/>
+							</Card.Text>
+						</Col>
+					</Row>
+
+					<Row xs={2} className="mt-2 text-muted">
+						<Col>
+							<Card.Text>
+								<i className="bi-thermometer-half" /> Pressure:{' '}
+								{current?.main.pressure}hPa
 							</Card.Text>
 						</Col>
 
 						<Col>
-							{current?.weather[0].icon && (
-								<Image
-									className="d-block mx-auto"
-									src={`https://openweathermap.org/img/wn/${current?.weather[0].icon}@2x.png`}
-									alt=""
-								/>
-							)}
+							<Card.Text>
+								<i className="bi-wind" /> Wind: {current?.wind.speed}
+								{weatherUnits.speed[units]}
+							</Card.Text>
 						</Col>
+
+						<Col>
+							<Card.Text>
+								<i className="bi-moisture" /> Humidity: {current?.main.humidity}
+								%
+							</Card.Text>
+						</Col>
+
+						<Col>
+							<Card.Text>
+								<i className="bi-cloud-haze2" /> Visibility:{' '}
+								{current?.visibility! / 1000}km
+							</Card.Text>
+						</Col>
+
+						{current?.rain && (
+							<Col>
+								<Card.Text>
+									<i className="bi-umbrella" /> Rain: {current?.rain['1h']}mm
+								</Card.Text>
+							</Col>
+						)}
+
+						{current?.snow && (
+							<Col>
+								<Card.Text>
+									<i className="bi-snow" /> Snow: {current?.snow['1h']}mm
+								</Card.Text>
+							</Col>
+						)}
 					</Row>
 				</Container>
 			</Card.Body>
-
-			<ListGroup variant="flush">
-				<ListGroup.Item className="d-flex justify-content-between">
-					<span>Humidity:</span>
-					<span>{current?.main.humidity}%</span>
-				</ListGroup.Item>
-
-				<ListGroup.Item className="d-flex justify-content-between">
-					<span>Pressure:</span>
-					<span>{current?.main.pressure}hPa</span>
-				</ListGroup.Item>
-
-				<ListGroup.Item className="d-flex justify-content-between">
-					<span>Visibility:</span>
-					<span>{current?.visibility! / 1000}km</span>
-				</ListGroup.Item>
-			</ListGroup>
 		</Card>
 	);
 };
