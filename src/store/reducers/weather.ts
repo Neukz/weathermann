353aca8e994c8cfc0200d374c6f46915a93a213f
Kacper Neumann <kapilek31@gmail.com>
2022-12-27 13:11:04 +0100

    Remove unused response fields from type declarations

--- src/store/reducers/weather.ts
+++ src/store/reducers/weather.ts
@@ -8,66 +8,40 @@ import { timeOfDay } from '../../constants/timeOfDay';
 
 // Responses from OpenWeatherMap API
 interface CurrentWeather {
-	coord: {
-		lon: number;
-		lat: number;
-	};
 	weather: {
-		id: number;
 		main: string;
 		description: string;
 		icon: string;
 	}[];
-	base: string;
 	main: {
 		temp: number;
 		feels_like: number;
-		temp_min: number;
-		temp_max: number;
 		pressure: number;
 		humidity: number;
-		sea_level: number;
-		grnd_level: number;
 	};
 	visibility: number;
 	wind: {
 		speed: number;
-		deg: number;
-		gust: number;
-	};
-	clouds: {
-		all: number;
 	};
 	rain: {
 		'1h': number;
-		'3h': number;
 	};
 	snow: {
 		'1h': number;
-		'3h': number;
 	};
 	dt: number;
 	sys: {
-		type: number;
-		id: number;
 		country: string;
 		sunrise: number;
 		sunset: number;
 	};
 	timezone: number;
-	id: number;
 	name: string;
-	cod: number;
 }
 
 interface Air {
-	coord: {
-		lon: number;
-		lat: number;
-	};
 	list: [
 		{
-			dt: number;
 			main: {
 				aqi: number;
 			};
@@ -86,42 +60,13 @@ interface Air {
 }
 
 interface Forecast {
-	cod: string;
-	message: number;
-	cnt: number;
 	list: [
 		{
-			dt: number;
-			main: CurrentWeather['main'] & {
-				temp_kf: number;
-			};
+			main: CurrentWeather['main'];
 			weather: CurrentWeather['weather'];
-			clouds: CurrentWeather['clouds'];
-			wind: CurrentWeather['wind'];
-			visibility: number;
-			pop: number;
-			rain: {
-				'3h': number;
-			};
-			sys: {
-				pod: 'd' | 'n';
-			};
 			dt_txt: string;
 		}
 	];
-	city: {
-		id: number;
-		name: string;
-		coord: {
-			lat: number;
-			lon: number;
-		};
-		country: string;
-		population: number;
-		timezone: number;
-		sunrise: number;
-		sunset: number;
-	};
 }
 
 // Forecast data grouped by day
